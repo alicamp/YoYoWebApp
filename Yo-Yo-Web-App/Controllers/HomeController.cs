2da10b894dfbf96b686c0b0621939f714c6aa4d1
Abdeali Vohra <abdealivohra@192.168.1.3>
2023-07-11 20:46:04 +0530

    initial commit

---
+++ Yo-Yo-Web-App/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Yo_Yo_Web_App.Models;
+
+
+namespace Yo_Yo_Web_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public JsonResult GetYoYoTest()
+        {
+            string _rootPath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            _rootPath = _rootPath.Substring(0, _rootPath.IndexOf("bin"));
+            string _filePath = $"{_rootPath}YoyoTest.json";
+            List<YoYoClass> Items = new List<YoYoClass>();
+            string json = string.Empty;
+            using (StreamReader r = new StreamReader(_filePath))
+            {
+                json = r.ReadToEnd();
+                Items = Newtonsoft.Json.JsonConvert.DeserializeObject<List<YoYoClass>>(json);
+            }
+
+            return Json(Items);
+        }
+    }
+}
---
+++ Yo-Yo-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yo_Yo_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
